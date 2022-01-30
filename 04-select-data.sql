a75af7683f2e6e74b059436ef8959d61017fb773
Rafael Gayoso <gayoso0597@gmail.com>
2022-01-30 21:24:30 +0100

    Added Default Values

--- 04-select-data.sql
+++ 04-select-data.sql
@@ -1,2 +1,2 @@
 SELECT *
-from employers;
+from conversations;
---
+++ 08-default-values.sql
@@ -0,0 +1,14 @@
+CREATE TABLE employers (
+    company_name VARCHAR(255),
+    company_address VARCHAR(255),
+    -- yearly_revenue FLOAT (5,2) -- Aproximation, Allowed: 123.12, 12.1, Not Allowed: 1234.12, 1.123
+    yearly_revenue NUMERIC(5, 2),
+    -- Exact Value, Allowed:123.12, Not Allowed: 12345.123
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+CREATE TABLE conversations (
+    user_name VARCHAR(255),
+    employer_name VARCHAR(255),
+    message TEXT,
+    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
